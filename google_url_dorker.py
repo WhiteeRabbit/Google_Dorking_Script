8f29fd20b9a628d73f090960704149571b02b870
White__RABBIT <143087729+WhiteeRabbit@users.noreply.github.com>
2025-06-24 13:14:40 +0400

    Add files via upload

---
+++ google_url_dorker.py
@@ -0,0 +1,46 @@
+# inurl:"/xmlrpc.php?rsd" & ext:php
+from googlesearch import search
+import argparse
+
+parser = argparse.ArgumentParser(description="Tool to extract URLs using Google Dork. "+"""python3 google_dorker.py --dork 'inurl:"/xmlrpc.php?rsd" & ext:php' --count 50""")
+parser.add_argument('-d', '--dork',required=True, help="Google dork query")
+parser.add_argument('-c', '--count', default=10, type=int, help="Number of URLs to fetch (default: 10)")
+parser.add_argument('-w', '--write', type=str, help="Save results to a text file")
+parser.add_argument('-x', '--examples', action='store_true', help="Show usage examples and Contacts")
+
+args = parser.parse_args()
+
+
+links = []
+
+proxy = 'http://72.10.160.91:20137'
+
+def dorksearch(query, count):
+    res = search(query, num_results=count, proxy=proxy, timeout=22)
+    
+    for url in res:
+        links.append(url + '\n')
+        print(url)
+        
+        
+def save(w):
+    with open(str(w), "w") as f:
+        f.writelines(links)
+        print("Succesfully written!")
+        
+
+
+if args.examples:
+    print("""Examples:
+    python3 google_dorker.py --dork 'inurl:"/xmlrpc.php?rsd" & ext:php' -w results.txt -c 50
+    python3 google_dorker.py --dork 'inurl:"/xmlrpc.php?rsd" & ext:php' --count 50
+    python3 google_dorker.py -d 'inurl:"/xmlrpc.php?rsd" & ext:php' -c 20 -w res.txt -s 20
+    Thanks for using this tool!
+    My Github: https://github.com/WhiteeRabbit""")
+    exit(0)
+
+if args.dork:
+    dorksearch(args.dork, args.count)
+    
+if args.write:
+    save(args.write)
